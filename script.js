b4838f6be93b2ecee2a6dc8703e5adef6bfae1b2
Moses Butera <152932522+MosesButera@users.noreply.github.com>
2025-08-14 08:50:28 -0600

    Add operate() function to handle operator logic Uses a switch statement to link a given operator string to the corresponding arithmetic function. This centralizes operation selection, improving code readability.

--- script.js
+++ script.js
@@ -13,3 +13,18 @@ function multiply(x, y){
 function divide(x, y){
     return x / y;
 }
+
+function operate(operator, number1, number2){
+    switch(operator){
+        case "add":
+            return add(number1, number2);
+        case "subtract":
+            return subtract(number1, number2); 
+        case "multiply":
+            return multiply(number1, number2);
+        case "divide":
+            return divide(number1, number2);
+        default: 
+            return "Unknown operator";
+    }
+}
